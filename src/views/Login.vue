83bb738a64d37d4b8978f7ba7baffb06fdff472d
bteo <tteo43@gmail.com>
2021-03-09 23:50:00 +0800

    updated facebook login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -90,8 +90,7 @@ export default {
             var provider = new firebase.auth.FacebookAuthProvider();
 
             if (isMobile) {
-                this.facebookSignInPopup(provider);
-                //firebase.auth().signInWithRedirect(provider);
+                firebase.auth().signInWithRedirect(provider);
             } else {
                 this.facebookSignInPopup(provider);
             }
@@ -102,27 +101,28 @@ export default {
                 .auth()
                 .signInWithPopup(provider)
                 .then((result) => {
-                /** @type {firebase.auth.OAuthCredential} */
-                var credential = result.credential;
+                    /** @type {firebase.auth.OAuthCredential} */
+                    var credential = result.credential;
 
-                // The signed-in user info.
-                var user = result.user;
+                    // The signed-in user info.
+                    var user = result.user;
 
-                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                var accessToken = credential.accessToken;
-
-                // ...
+                    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+                    var accessToken = credential.accessToken;
+                    console.log("Pop up");
+                    console.log(user);
+                    // ...
                 })
                 .catch((error) => {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
 
-                // ...
+                    // ...
                 });
             // [END auth_facebook_signin_popup]
         },
@@ -131,28 +131,33 @@ export default {
             firebase.auth()
                 .getRedirectResult()
                 .then((result) => {
-                if (result.credential) {
-                    /** @type {firebase.auth.OAuthCredential} */
-                    var credential = result.credential;
+                    if (result.credential) {
+                        /** @type {firebase.auth.OAuthCredential} */
+                        var credential = result.credential;
 
-                    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                    var token = credential.accessToken;
+                        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+                        var token = credential.accessToken;
+                        // ...
+                        console.log("Redirect Result");
+                        console.log(result);
+                    }
+                    // The signed-in user info.
+                    var user = result.user;
+                    }).catch((error) => {
+                    // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
                     // ...
-                }
-                // The signed-in user info.
-                var user = result.user;
-                }).catch((error) => {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
                 });
             // [END auth_facebook_signin_redirect_result]
             }
+    },
+    created: function() {
+        this.facebookSignInRedirectResult();
     },
     computed: {
         headerStyle() {
