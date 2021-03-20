5da00eed2e53a610ce1ad6e7d9a3a54ea0d55f49
bteo <tteo43@gmail.com>
2021-03-20 16:34:34 +0800

    updated footer

--- src/layout/MainFooter.vue
+++ src/layout/MainFooter.vue
@@ -5,30 +5,6 @@
     :data-background-color="backgroundColor"
   >
     <div class="container">
-      <nav>
-        <ul>
-          <li>
-            <a href="https://www.creative-tim.com">
-              Creative Tim
-            </a>
-          </li>
-          <li>
-            <a href="https:///presentation.creative-tim.com">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a href="https:///blog.creative-tim.com">
-              Blog
-            </a>
-          </li>
-          <li>
-            <a href="https://www.creative-tim.com/license">
-              Licenses
-            </a>
-          </li>
-        </ul>
-      </nav>
       <div class="copyright">
         MakanSaviour &copy; {{ year }} 
       </div>
