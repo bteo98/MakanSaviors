8875ac1dbf964dfab115f6282a1e04afeee5ac41
bteo <tteo43@gmail.com>
2021-03-23 12:31:27 +0800

    updated explore card

--- src/views/components/ExploreCard.vue
+++ src/views/components/ExploreCard.vue
@@ -1,18 +1,40 @@
 <template>
-    <md-card>
+    <md-card id="explore-card">
         <md-card-content>
-            <img v-bind:src="imgRef" v-bind:alt="description['name']">
-            <p>
-                Food Description: {{description['name']}}<br>
-                Donor Name: {{
-                    profile['firstName'].charAt(0).toUpperCase() 
-                        + profile['firstName'].slice(1).toLowerCase() 
-                        + " " + profile['lastName'].charAt(0).toUpperCase()
-                        + profile['lastName'].slice(1).toLowerCase()}}<br>
-                Collection Location: {{}}<br>
-                Quantity Avaliable: {{description["quantity"]}}<br>
-                Expiry Date: {{description['expiryDate']}}
-            </p>
+            <div class="md-layout">
+                <div class="md-layout-item">
+                    <div class="container">
+                        <img
+                            v-bind:src="imgRef"
+                            v-bind:alt="description['name']"
+                            class="rounded"
+                            :class="{ 'responsive-image': responsive }"
+                        />
+                        <div class="text">
+                            <small>Food Description:</small>
+                            {{ description["name"] }}<br />
+                            <small>Donor Name:</small>
+                            {{
+                                profile["firstName"].charAt(0).toUpperCase() +
+                                    profile["firstName"]
+                                        .slice(1)
+                                        .toLowerCase() +
+                                    " " +
+                                    profile["lastName"]
+                                        .charAt(0)
+                                        .toUpperCase() +
+                                    profile["lastName"].slice(1).toLowerCase()
+                            }}<br />
+                            <small>Collection Location:</small>
+                            {{ profile["address"] }}<br />
+                            <small>Quantity Avaliable:</small>
+                            {{ description["quantity"] }}<br />
+                            <small>Expiry Date:</small>
+                            {{ description["expiryDate"].toLocaleString("en-US") }}
+                        </div>
+                    </div>
+                </div>
+            </div>
             <slot name="content"></slot>
         </md-card-content>
     </md-card>
@@ -31,6 +53,7 @@ export default {
             description: {},
             profile: {},
             expiryDate: "",
+            responsive: false,
         };
     },
     methods: {
@@ -47,7 +70,7 @@ export default {
             var database = firebase.firestore();
 
             database
-                .collection('users')
+                .collection("users")
                 .doc(this.UID)
                 .get()
                 .then((items) => {
@@ -69,16 +92,52 @@ export default {
 
                     for (let [key, val] of Object.entries(items.data())) {
                         console.log(key + " " + val);
-                        val = key == "expiryDate" ?
-                            new Date(val.toDate()) : val;
+                        val =
+                            key == "expiryDate" ? new Date(val.toDate()) : val;
                         data[key] = val;
                     }
                     this.description = data;
                 });
         },
+        onResponsiveInverted() {
+            if (window.innerWidth < 600) {
+                this.responsive = true;
+            } else {
+                this.responsive = false;
+            }
+        },
     },
     created() {
         this.fetchItems();
     },
+    mounted() {
+        this.onResponsiveInverted();
+        window.addEventListener("resize", this.onResponsiveInverted);
+    },
+    beforeDestroy() {
+        window.removeEventListener("resize", this.onResponsiveInverted);
+    },
 };
 </script>
+<style scoped>
+small,
+div {
+    font-size: medium;
+}
+
+img {
+    display: inline-block;
+    width: 45% !important; 
+}
+
+.text {
+    display: inline-block;
+    width: 45%;
+    padding: 26px 0;
+}
+
+#explore-card {
+    max-width: 580px !important;
+    width: 100%;
+}
+</style>
