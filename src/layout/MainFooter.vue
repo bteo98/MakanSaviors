a7bfdfa47fa39f42f82baeeccae7a2829d4fbf37
Daryl Tan <daryltan900@gmail.com>
2021-04-12 23:54:26 +0800

    changed images on landing page, updated styling on footer

--- src/layout/MainFooter.vue
+++ src/layout/MainFooter.vue
@@ -1,25 +1,25 @@
 <template>
-  <footer
-    class="footer"
-    :class="{ [`footer-${type}`]: type }"
-    :data-background-color="backgroundColor"
-  >
-    <div class="container">
-      <div class="copyright">MakanSaviour &copy; {{ year }}</div>
-    </div>
-  </footer>
+	<footer
+		class="footer"
+		:class="{[`footer-${type}`]: type}"
+		:data-background-color="backgroundColor"
+	>
+		<div class="container">
+			<div class="copyright">MakanSaviors &copy; {{ year }}</div>
+		</div>
+	</footer>
 </template>
 <script>
 export default {
-  props: {
-    backgroundColor: String,
-    type: String
-  },
-  data() {
-    return {
-      year: new Date().getFullYear()
-    };
-  }
+	props: {
+		backgroundColor: String,
+		type: String,
+	},
+	data() {
+		return {
+			year: new Date().getFullYear(),
+		};
+	},
 };
 </script>
 <style></style>
