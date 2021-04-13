49239686cd636b8ec9e50880e4b3d8b609131654
Daryl Tan <daryltan900@gmail.com>
2021-04-14 01:19:05 +0800

    change images on site

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,123 +1,119 @@
 <template>
-  <div class="wrapper" style="height: 100vh;">
-    <div class="section page-header header-filter" :style="headerStyle">
-      <div class="container">
-        <div class="md-layout">
-          <div
-            class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
-          >
-            <login-card header-color="green">
-              <h4 slot="title" class="card-title">Login</h4>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>email</md-icon>
-                <label>Email...</label>
-                <md-input v-model="email" type="email"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>lock_outline</md-icon>
-                <label>Password...</label>
-                <md-input type="password" v-model="password"></md-input>
-              </md-field>
+	<div class="wrapper" style="height: 100vh;">
+		<div class="section page-header header-filter" :style="headerStyle">
+			<div class="container">
+				<div class="md-layout">
+					<div
+						class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
+					>
+						<login-card header-color="green">
+							<h4 slot="title" class="card-title">Login</h4>
+							<md-field class="md-form-group" slot="inputs">
+								<md-icon>email</md-icon>
+								<label>Email...</label>
+								<md-input v-model="email" type="email"></md-input>
+							</md-field>
+							<md-field class="md-form-group" slot="inputs">
+								<md-icon>lock_outline</md-icon>
+								<label>Password...</label>
+								<md-input type="password" v-model="password"></md-input>
+							</md-field>
 
-              <p slot="description" class="description">
-                New to MakanSaviors?
-                <a href="#/signup">Sign Up</a>
-              </p>
+							<p slot="description" class="description">
+								New to MakanSaviors?
+								<a href="#/signup">Sign Up</a>
+							</p>
 
-              <div slot="errors" class="errors" v-if="errors.length">
-                <br />
-                <p>
-                  <b>Please correct the following error(s):</b>
-                </p>
-                <ul>
-                  <li :key="error.id" v-for="error in errors">
-                    {{ error }}
-                  </li>
-                </ul>
-              </div>
-              <md-button
-                slot="footer"
-                class="md-simple md-success md-lg"
-                v-on:click="forgetPassword"
-              >
-                Forget Password
-              </md-button>
-              <md-button
-                slot="footer"
-                class="md-simple md-success md-lg"
-                v-on:click="login"
-              >
-                Login
-              </md-button>
-            </login-card>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+							<div slot="errors" class="errors" v-if="errors.length">
+								<br />
+								<p>
+									<b>Please correct the following error(s):</b>
+								</p>
+								<ul>
+									<li :key="error.id" v-for="error in errors">
+										{{ error }}
+									</li>
+								</ul>
+							</div>
+							<md-button
+								slot="footer"
+								class="md-simple md-success md-lg"
+								v-on:click="forgetPassword"
+							>
+								Forget Password
+							</md-button>
+							<md-button slot="footer" class="md-simple md-success md-lg" v-on:click="login">
+								Login
+							</md-button>
+						</login-card>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { LoginCard } from "@/components";
-import { isMobile } from "mobile-device-detect";
+import {LoginCard} from "@/components";
+import {isMobile} from "mobile-device-detect";
 import firebase from "../firebase.js";
 
 export default {
-  components: {
-    LoginCard
-  },
-  bodyClass: "login-page",
-  data() {
-    return {
-      username: null,
-      email: null,
-      password: null,
-      errors: []
-    };
-  },
-  props: {
-    header: {
-      type: String,
-      default: require("@/assets/img/profile_city.jpg")
-    }
-  },
-  methods: {
-    forgetPassword() {
-      this.$router.push("/resetpassword");
-    },
-    login() {
-      this.errors = [];
-      if (!this.email) {
-        this.errors.push("Email required");
-      }
-      if (!this.password) {
-        this.errors.push("Password required");
-      }
-      if (!this.errors.length) {
-        firebase
-          .auth()
-          .signInWithEmailAndPassword(this.email, this.password)
-          .then(user => {
-            this.$store.commit("change");
-            let path = `/profile/${firebase.auth().currentUser.uid}`;
-            this.$router.push({
-              path: path
-            });
-          })
-          .catch(error => {
-            this.errors.push(error);
-          });
-      }
-    }
-  },
-  created: function() {},
-  computed: {
-    headerStyle() {
-      return {
-        backgroundImage: `url(${this.header})`
-      };
-    }
-  }
+	components: {
+		LoginCard,
+	},
+	bodyClass: "login-page",
+	data() {
+		return {
+			username: null,
+			email: null,
+			password: null,
+			errors: [],
+		};
+	},
+	props: {
+		header: {
+			type: String,
+			default: require("@/assets/img/food5.jpg"),
+		},
+	},
+	methods: {
+		forgetPassword() {
+			this.$router.push("/resetpassword");
+		},
+		login() {
+			this.errors = [];
+			if (!this.email) {
+				this.errors.push("Email required");
+			}
+			if (!this.password) {
+				this.errors.push("Password required");
+			}
+			if (!this.errors.length) {
+				firebase
+					.auth()
+					.signInWithEmailAndPassword(this.email, this.password)
+					.then((user) => {
+						this.$store.commit("change");
+						let path = `/profile/${firebase.auth().currentUser.uid}`;
+						this.$router.push({
+							path: path,
+						});
+					})
+					.catch((error) => {
+						this.errors.push(error);
+					});
+			}
+		},
+	},
+	created: function() {},
+	computed: {
+		headerStyle() {
+			return {
+				backgroundImage: `url(${this.header})`,
+			};
+		},
+	},
 };
 </script>
 
--- src/views/RequestListing.vue
+++ src/views/RequestListing.vue
@@ -1,242 +1,235 @@
 <template>
-  <div class="wrapper" style="min-width: 875px;">
-    <parallax class="section header-filter" :style="headerStyle"> </parallax>
-    <div class="main main-raised" style="min-width: 820px;">
-      <div class="section">
-        <div class="container">
-          <div class="md-layout">
-            <div class="md-layout-item">
-              <div class="title">
-                <h3>Request</h3>
-              </div>
-              <div class="md-layout">
-                <div class="md-layout-item md-small-size-100">
-                  <tabs
-                    :tab-name="['As Donor', 'As Savior', 'Saved']"
-                    :tab-icon="['call_made', 'call_received', 'favorite']"
-                    plain
-                    flex-column
-                    nav-pills-icons
-                    color-button="success"
-                  >
-                    <!-- here you can add your content for tab-content -->
-                    <template slot="tab-pane-1">
-                      <div>
-                        <ul v-if="!processing" id="itemsList">
-                          <li
-                            class="md-layout"
-                            v-for="(item, index) in donorCollections"
-                            :key="index"
-                          >
-                            <div class="md-layout" style="padding-right: 5%;">
-                              <RequestCard
-                                class="md-layout-item requestcard"
-                                :data="item"
-                                :requestView="false"
-                              ></RequestCard>
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                    </template>
-                    <template slot="tab-pane-2">
-                      <div>
-                        <ul v-if="!processing" id="itemsList">
-                          <li
-                            class="md-layout"
-                            v-for="(item, index) in requestCollections"
-                            :key="index"
-                          >
-                            <div class="md-layout" style="padding-right: 5%;">
-                              <RequestCard
-                                class="md-layout-item requestcard"
-                                :data="item"
-                                :requestView="true"
-                              ></RequestCard>
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                    </template>
-                    <template slot="tab-pane-3">
-                      <div>
-                        <ul v-if="!processing" id="itemsList">
-                          <li
-                            class="md-layout"
-                            v-for="(item, index) in savedCollections"
-                            :key="index"
-                          >
-                            <div class="md-layout" style="padding-right: 5%;">
-                              <SavedCard
-                                class="md-layout-item requestcard mx-auto"
-                                :data="item"
-                              ></SavedCard>
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                    </template>
-                  </tabs>
-                </div>
-              </div>
-            </div>
-          </div>
-          <br /><br /><br />
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="wrapper" style="min-width: 875px;">
+		<parallax class="section header-filter" :style="headerStyle"> </parallax>
+		<div class="main main-raised" style="min-width: 820px;">
+			<div class="section">
+				<div class="container">
+					<div class="md-layout">
+						<div class="md-layout-item">
+							<div class="title">
+								<h3>Request</h3>
+							</div>
+							<div class="md-layout">
+								<div class="md-layout-item md-small-size-100">
+									<tabs
+										:tab-name="['As Donor', 'As Savior', 'Saved']"
+										:tab-icon="['call_made', 'call_received', 'favorite']"
+										plain
+										flex-column
+										nav-pills-icons
+										color-button="success"
+									>
+										<!-- here you can add your content for tab-content -->
+										<template slot="tab-pane-1">
+											<div>
+												<ul v-if="!processing" id="itemsList">
+													<li
+														class="md-layout"
+														v-for="(item, index) in donorCollections"
+														:key="index"
+													>
+														<div class="md-layout" style="padding-right: 5%;">
+															<RequestCard
+																class="md-layout-item requestcard"
+																:data="item"
+																:requestView="false"
+															></RequestCard>
+														</div>
+													</li>
+												</ul>
+											</div>
+										</template>
+										<template slot="tab-pane-2">
+											<div>
+												<ul v-if="!processing" id="itemsList">
+													<li
+														class="md-layout"
+														v-for="(item, index) in requestCollections"
+														:key="index"
+													>
+														<div class="md-layout" style="padding-right: 5%;">
+															<RequestCard
+																class="md-layout-item requestcard"
+																:data="item"
+																:requestView="true"
+															></RequestCard>
+														</div>
+													</li>
+												</ul>
+											</div>
+										</template>
+										<template slot="tab-pane-3">
+											<div>
+												<ul v-if="!processing" id="itemsList">
+													<li
+														class="md-layout"
+														v-for="(item, index) in savedCollections"
+														:key="index"
+													>
+														<div class="md-layout" style="padding-right: 5%;">
+															<SavedCard
+																class="md-layout-item requestcard mx-auto"
+																:data="item"
+															></SavedCard>
+														</div>
+													</li>
+												</ul>
+											</div>
+										</template>
+									</tabs>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br /><br /><br />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import firebase from "../firebase.js";
 import RequestCard from "./components/RequestCard";
 import SavedCard from "./components/SavedCard";
-import { Tabs } from "@/components";
+import {Tabs} from "@/components";
 
 export default {
-  bodyClass: "request-listing",
-  data() {
-    return {
-      donorCollections: [],
-      requestCollections: [],
-      savedCollections: [],
-      processing: true,
-      header: require("@/assets/img/city-profile.jpg")
-    };
-  },
-  props: {
-    userID: { type: String }
-  },
-  components: {
-    RequestCard,
-    SavedCard,
-    Tabs
-  },
-  computed: {
-    headerStyle() {
-      return {
-        backgroundImage: `url(${this.header})`
-      };
-    }
-  },
-  methods: {
-    donorLiveFetch: function() {
-      var db = firebase.firestore();
-      let collect = "donorRequest/" + this.userID + "/foodDonated";
+	bodyClass: "request-listing",
+	data() {
+		return {
+			donorCollections: [],
+			requestCollections: [],
+			savedCollections: [],
+			processing: true,
+			header: require("@/assets/img/fruits4.jpg"),
+		};
+	},
+	props: {
+		userID: {type: String},
+	},
+	components: {
+		RequestCard,
+		SavedCard,
+		Tabs,
+	},
+	computed: {
+		headerStyle() {
+			return {
+				backgroundImage: `url(${this.header})`,
+			};
+		},
+	},
+	methods: {
+		donorLiveFetch: function() {
+			var db = firebase.firestore();
+			let collect = "donorRequest/" + this.userID + "/foodDonated";
 
-      db.collection(collect)
-        .orderBy("timeRequested", "desc")
-        .onSnapshot(snapshot => {
-          this.donorCollections = [];
+			db.collection(collect)
+				.orderBy("timeRequested", "desc")
+				.onSnapshot((snapshot) => {
+					this.donorCollections = [];
 
-          snapshot.forEach(doc => {
-            let data = {};
-            data["foodID"] = doc.id;
-            doc = doc.data();
-            data["listingName"] = doc.listingName;
-            data["saviorID"] = doc.saviorID;
-            data["status"] = doc.status;
-            data["timeRequested"] = new Date(
-              doc.timeRequested.toDate().toLocaleString("en-US")
-            );
-            data["donorID"] = this.userID;
-            data["userID"] = this.userID;
-            this.donorCollections.push(data);
-          });
-          this.processing = false;
-        });
-    },
-    requestLiveFetch: function() {
-      var db = firebase.firestore();
-      let collect = "donorRequest/" + this.userID + "/foodRequested";
+					snapshot.forEach((doc) => {
+						let data = {};
+						data["foodID"] = doc.id;
+						doc = doc.data();
+						data["listingName"] = doc.listingName;
+						data["saviorID"] = doc.saviorID;
+						data["status"] = doc.status;
+						data["timeRequested"] = new Date(doc.timeRequested.toDate().toLocaleString("en-US"));
+						data["donorID"] = this.userID;
+						data["userID"] = this.userID;
+						this.donorCollections.push(data);
+					});
+					this.processing = false;
+				});
+		},
+		requestLiveFetch: function() {
+			var db = firebase.firestore();
+			let collect = "donorRequest/" + this.userID + "/foodRequested";
 
-      db.collection(collect)
-        .orderBy("timeRequested", "desc")
-        .onSnapshot(snapshot => {
-          this.requestCollections = [];
+			db.collection(collect)
+				.orderBy("timeRequested", "desc")
+				.onSnapshot((snapshot) => {
+					this.requestCollections = [];
 
-          snapshot.forEach(doc => {
-            let data = {};
-            data["foodID"] = doc.id;
-            doc = doc.data();
-            data["listingName"] = doc.listingName;
-            data["saviorID"] = this.userID;
-            data["status"] = doc.status;
-            data["timeRequested"] = new Date(
-              doc.timeRequested.toDate().toLocaleString("en-US")
-            );
-            data["donorID"] = doc.donorID;
-            data["userID"] = this.userID;
-            this.requestCollections.push(data);
-          });
-          this.processing = false;
-        });
-    },
-    saveLiveFetch: function() {
-      var db = firebase.firestore();
-      let collect = "donorRequest/" + this.userID + "/foodSaved";
+					snapshot.forEach((doc) => {
+						let data = {};
+						data["foodID"] = doc.id;
+						doc = doc.data();
+						data["listingName"] = doc.listingName;
+						data["saviorID"] = this.userID;
+						data["status"] = doc.status;
+						data["timeRequested"] = new Date(doc.timeRequested.toDate().toLocaleString("en-US"));
+						data["donorID"] = doc.donorID;
+						data["userID"] = this.userID;
+						this.requestCollections.push(data);
+					});
+					this.processing = false;
+				});
+		},
+		saveLiveFetch: function() {
+			var db = firebase.firestore();
+			let collect = "donorRequest/" + this.userID + "/foodSaved";
 
-      db.collection(collect).onSnapshot(snapshot => {
-        this.savedCollections = [];
+			db.collection(collect).onSnapshot((snapshot) => {
+				this.savedCollections = [];
 
-        snapshot.forEach(doc => {
-          let data = {};
-          data["foodID"] = doc.id;
-          doc = doc.data();
-          data["listingName"] = doc.listingName;
-          data["saviorID"] = this.userID;
-          data["location"] = doc.location;
-          data["quantity"] = doc.quantity;
-          data["donorID"] = doc.donorID;
-          data["status"] = doc.status;
-          data["expiry"] = doc.expiry;
-          data["userID"] = this.userID;
+				snapshot.forEach((doc) => {
+					let data = {};
+					data["foodID"] = doc.id;
+					doc = doc.data();
+					data["listingName"] = doc.listingName;
+					data["saviorID"] = this.userID;
+					data["location"] = doc.location;
+					data["quantity"] = doc.quantity;
+					data["donorID"] = doc.donorID;
+					data["status"] = doc.status;
+					data["expiry"] = doc.expiry;
+					data["userID"] = this.userID;
 
-          this.savedCollections.push(data);
-        });
-        this.processing = false;
-      });
-    },
-    checkPath() {
-      window.onpopstate = event => {
-        if (
-          this.$route.path == "/login" ||
-          this.$route.path == "/createaccount"
-        ) {
-          let path = `/requestlisting`;
+					this.savedCollections.push(data);
+				});
+				this.processing = false;
+			});
+		},
+		checkPath() {
+			window.onpopstate = (event) => {
+				if (this.$route.path == "/login" || this.$route.path == "/createaccount") {
+					let path = `/requestlisting`;
 
-          this.$router.push({
-            path: path
-          });
-        }
-      };
-    }
-  },
-  mounted() {
-    this.donorLiveFetch();
-    this.requestLiveFetch();
-    this.saveLiveFetch();
-    this.checkPath();
-  }
+					this.$router.push({
+						path: path,
+					});
+				}
+			};
+		},
+	},
+	mounted() {
+		this.donorLiveFetch();
+		this.requestLiveFetch();
+		this.saveLiveFetch();
+		this.checkPath();
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 requestcard {
-  padding: 500px 500px !important;
+	padding: 500px 500px !important;
 }
 
 ul {
-  display: flex;
-  flex-wrap: wrap;
-  list-style-type: none;
-  padding: 0;
+	display: flex;
+	flex-wrap: wrap;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  flex-grow: 1;
-  flex-basis: 300px;
-  padding: 10px;
-  margin: 10px;
+	flex-grow: 1;
+	flex-basis: 300px;
+	padding: 10px;
+	margin: 10px;
 }
 </style>
