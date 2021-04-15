87c7a8b0bf2949731f2a13da19c55a854a0846c4
Daryl Tan <daryltan900@gmail.com>
2021-04-16 02:40:59 +0800

    changed graph styling

--- src/charts/FoodWasteChart.js
+++ src/charts/FoodWasteChart.js
@@ -1,93 +1,86 @@
-import { Line } from "vue-chartjs";
+import {Line} from "vue-chartjs";
 import firebase from "../firebase";
 
 export default {
-  extends: Line,
-  data: function() {
-    return {
-      datacollection: {
-        labels: [],
-        datasets: [
-          {
-            label: "Amount of food waste (tonnes)",
-            borderColor: "#FC2525",
-            pointBackgroundColor: "#FC2525",
-            borderWidth: 1,
-            pointBorderColor: "#FC2525",
-            backgroundColor: null,
-            data: []
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: "Monthly Amount of Food Saved",
-          fontColor: "#212733"
-        },
-        legend: {
-          labels: {
-            fontColor: "#212733"
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                min: 400000,
-                max: 900000,
-                fontColor: "#212733",
-                beginAtZero: true,
-                callback: function(value) {
-                  if (value % 1 === 0) {
-                    return value;
-                  }
-                }
-              }
-            }
-          ],
-          xAxes: [
-            {
-              ticks: {
-                fontColor: "#212733"
-              }
-            }
-          ]
-        }
-      }
-    };
-  },
-  methods: {
-    fetchItems() {
-      var db = firebase.firestore();
-      db.collection("overallData")
-        .doc("country")
-        .get()
-        .then(doc => {
-          var data = doc.data();
-          this.datacollection.labels = data["year"];
-          this.datacollection.datasets[0].data = data["generated"];
-        })
-        .then(() => {
-          this.renderChart(this.datacollection, this.options);
-        });
-    },
-    setBackgroundColor() {
-      let gradient1 = this.$refs.canvas
-        .getContext("2d")
-        .createLinearGradient(0, 0, 0, 450);
-      gradient1.addColorStop(0, "rgba(255, 0,0, 0.5)");
-      gradient1.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
-      gradient1.addColorStop(1, "rgba(255, 0, 0, 0)");
+	extends: Line,
+	data: function() {
+		return {
+			datacollection: {
+				labels: [],
+				datasets: [
+					{
+						label: "Amount of food waste (tonnes)",
+						borderColor: "#FC2525",
+						pointBackgroundColor: "#FC2525",
+						borderWidth: 1,
+						pointBorderColor: "#FC2525",
+						backgroundColor: null,
+						data: [],
+					},
+				],
+			},
+			options: {
+				legend: {
+					labels: {
+						fontColor: "#212733",
+					},
+				},
+				responsive: true,
+				maintainAspectRatio: false,
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								min: 400000,
+								max: 900000,
+								fontColor: "#212733",
+								beginAtZero: true,
+								callback: function(value) {
+									if (value % 1 === 0) {
+										return value;
+									}
+								},
+							},
+						},
+					],
+					xAxes: [
+						{
+							ticks: {
+								fontColor: "#212733",
+							},
+						},
+					],
+				},
+			},
+		};
+	},
+	methods: {
+		fetchItems() {
+			var db = firebase.firestore();
+			db.collection("overallData")
+				.doc("country")
+				.get()
+				.then((doc) => {
+					var data = doc.data();
+					this.datacollection.labels = data["year"];
+					this.datacollection.datasets[0].data = data["generated"];
+				})
+				.then(() => {
+					this.renderChart(this.datacollection, this.options);
+				});
+		},
+		setBackgroundColor() {
+			let gradient1 = this.$refs.canvas.getContext("2d").createLinearGradient(0, 0, 0, 450);
+			gradient1.addColorStop(0, "rgba(255, 0,0, 0.5)");
+			gradient1.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
+			gradient1.addColorStop(1, "rgba(255, 0, 0, 0)");
 
-      let dataset = this.datacollection.datasets;
-      dataset[0].backgroundColor = gradient1;
-    }
-  },
-  mounted() {
-    this.setBackgroundColor();
-    this.fetchItems();
-  }
+			let dataset = this.datacollection.datasets;
+			dataset[0].backgroundColor = gradient1;
+		},
+	},
+	mounted() {
+		this.setBackgroundColor();
+		this.fetchItems();
+	},
 };
--- src/charts/TotalListingsChart.js
+++ src/charts/TotalListingsChart.js
@@ -1,79 +1,80 @@
-import { Bar } from "vue-chartjs";
+import {Bar} from "vue-chartjs";
 import firebase from "../firebase";
 
 export default {
-  extends: Bar,
-  data: function() {
-    return {
-      datacollection: {
-        labels: [],
-        datasets: [
-          {
-            label: "Number of Listing on MakanSaviors",
-            borderWidth: 1,
-            borderColor: "black",
-            backgroundColor: "rgba(50, 153, 76, 0.7)",
-            fill: true,
-            data: []
-          }
-        ]
-      },
-      options: {
-        legend: { display: false },
-        title: {
-          display: false
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                min: 0,
-                max: 100
-              }
-            }
-          ],
-          xAxes: [
-            {
-              ticks: {
-                fontSize: 11
-              }
-            }
-          ]
-        }
-      }
-    };
-  },
-  methods: {
-    fetchItems() {
-      var db = firebase.firestore();
-      var dct = {};
-      var cat = [];
-      var numbers = [];
-      db.collection("overallData")
-        .doc("website")
-        .get()
-        .then(doc => {
-          var data = doc.data();
-          dct = data["numListings"];
-        })
-        .then(() => {
-          Object.entries(dct).forEach(([key, value]) => {
-            cat.push(key);
-          });
-          cat = cat.sort();
-          for (let i = 0; i < cat.length; i++) {
-            numbers.push(dct[cat[i]]);
-          }
-          this.datacollection.labels = cat;
-          this.datacollection.datasets[0].data = numbers;
-          this.renderChart(this.datacollection, this.options);
-        });
-    }
-  },
-  mounted() {
-    this.fetchItems();
-    // this.renderChart(this.datacollection, this.options);
-  }
+	extends: Bar,
+	data: function() {
+		return {
+			datacollection: {
+				labels: [],
+				datasets: [
+					{
+						label: "Number of Listing on MakanSaviors",
+						borderWidth: 1,
+						borderColor: "rgb(75, 192, 192)",
+						backgroundColor: "rgba(54, 162, 235, 0.2)",
+						fill: true,
+						data: [],
+						// barThickness: 70,
+					},
+				],
+			},
+			options: {
+				legend: {display: false},
+				title: {
+					display: false,
+				},
+				responsive: true,
+				maintainAspectRatio: false,
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								min: 0,
+								max: 100,
+							},
+						},
+					],
+					xAxes: [
+						{
+							ticks: {
+								fontSize: 11,
+							},
+						},
+					],
+				},
+			},
+		};
+	},
+	methods: {
+		fetchItems() {
+			var db = firebase.firestore();
+			var dct = {};
+			var cat = [];
+			var numbers = [];
+			db.collection("overallData")
+				.doc("website")
+				.get()
+				.then((doc) => {
+					var data = doc.data();
+					dct = data["numListings"];
+				})
+				.then(() => {
+					Object.entries(dct).forEach(([key, value]) => {
+						cat.push(key);
+					});
+					cat = cat.sort();
+					for (let i = 0; i < cat.length; i++) {
+						numbers.push(dct[cat[i]]);
+					}
+					this.datacollection.labels = cat;
+					this.datacollection.datasets[0].data = numbers;
+					this.renderChart(this.datacollection, this.options);
+				});
+		},
+	},
+	mounted() {
+		this.fetchItems();
+		// this.renderChart(this.datacollection, this.options);
+	},
 };
