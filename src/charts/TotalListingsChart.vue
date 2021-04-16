a9f3f2e254a4fcb4403d6b48808062dca8f4223c
bteo <tteo43@gmail.com>
2021-04-16 12:36:54 +0800

    fixed button on saved card

--- src/charts/TotalListingsChart.vue
+++ src/charts/TotalListingsChart.vue
@@ -1,19 +1,19 @@
 <template>
-	<div class="chart">
-		<h2 class="title text-center">
-			Number of Listings <br />
-			On MakanSaviors
-		</h2>
-		<Chart></Chart>
-	</div>
+  <div class="chart">
+    <h2 class="title text-center">
+      Number of Listings <br />
+      On MakanSaviors
+    </h2>
+    <Chart></Chart>
+  </div>
 </template>
 
 <script>
 import Chart from "./TotalListingsChart.js";
 export default {
-	components: {
-		Chart,
-	},
+  components: {
+    Chart
+  }
 };
 </script>
 
